0de663e6f210ca2ed9f4072078360698d94c7827
nando <fernando.garcia@the-cocktail.com>
2013-09-20 12:31:09 +0200

    .to_html simple test

--- test/google-tag-manager_test.rb
+++ test/google-tag-manager_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class GoogleTagManagerTest < ActiveSupport::TestCase
-  test "truth" do
+  test 'GoogleTagManager is a module' do
     assert_kind_of Module, GoogleTagManager
+  end
+
+  test '.to_html should return the gtm_id when defined' do
+    assert GoogleTagManager.to_html =~ /'#{GoogleTagManager::PLACEHOLDER_GTM_ID}'/
+    GoogleTagManager.gtm_id = gtm_id = 'GTM-1234567890'
+    assert GoogleTagManager.to_html =~ /'#{gtm_id}'/
+    assert (GoogleTagManager.to_html =~ /'#{GoogleTagManager::PLACEHOLDER_GTM_ID}'/).nil?
   end
 end
