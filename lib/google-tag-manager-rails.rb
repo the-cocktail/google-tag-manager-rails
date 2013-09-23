9d0c09ee6378cda6e8ad5a2ad9f5b902d35b8630
nando <fernando.garcia@the-cocktail.com>
2013-09-23 14:54:42 +0200

    let the gtm comments wrap the data layer

--- lib/google-tag-manager-rails.rb
+++ lib/google-tag-manager-rails.rb
@@ -15,7 +15,10 @@ module GoogleTagManager
     end
   
     def to_html
-      data_layer_tag + "\n" + container_tag
+      "<!-- Google Tag Manager -->\n" +
+      data_layer_tag + "\n" +
+      container_tag + "\n" +
+      "<!-- End Google Tag Manager -->"
     end
 
 
@@ -71,10 +74,8 @@ module GoogleTagManager
 
       def container_tag
         <<-HTML
-<!-- Google Tag Manager -->
 <noscript><iframe src="//www.googletagmanager.com/ns.html?id=#{gtm_id}" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','#{gtm_id}');</script>
-<!-- End Google Tag Manager -->
         HTML
       end
   end
