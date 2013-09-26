876a269176c6e6573b662c7d2b71cb1448b9da62
nando <fernando.garcia@the-cocktail.com>
2013-09-26 12:59:05 +0200

    debug mode fix

--- lib/google-tag-manager/rails/view_helpers.rb
+++ lib/google-tag-manager/rails/view_helpers.rb
@@ -32,7 +32,7 @@ $(document).ready(function() {
       });
       #{%!console.log('[GoogleTagManager] dataLayer.push({');! if GoogleTagManager.debug_mode }
       #{%!$.each(push_hash, function(k,v){ console.log("[GoogleTagManager]   '" + k + "': '" + v + "'")});! if GoogleTagManager.debug_mode }
-      #{%!console.log('[GoogleTagManager] });! if GoogleTagManager.debug_mode }
+      #{%!console.log('[GoogleTagManager] });');! if GoogleTagManager.debug_mode }
       dataLayer.push(push_hash);
     });
   });
